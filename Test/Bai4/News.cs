a2b05c06b5e02675c33656a8bc9580d1d576de13
Nguyễn Trọng Nghĩa <117567626+trongnghia10@users.noreply.github.com>
2023-01-04 09:49:17 +0700

    new update

---
+++ Test/Bai4/News.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai4
+{
+    class News : INews
+    {
+        private int id;
+        private String title, publishDate, author, content;
+        private float averageRate;
+
+        int[] rateList = new int[3];
+
+        //get set id
+        public int GetId()
+        {
+            return id;
+        }
+        public void SetId(int _id)
+        {
+            id = _id;
+        }
+        //get set title
+        public String GetTitle()
+        {
+            return title;
+        }
+        public void SetTitle(String _title)
+        {
+            title = _title;
+        }
+        // get set publishDate
+        public String GetPublishDate()
+        {
+            return publishDate;
+        }
+        public void SetPublishDate(String _publishDate)
+        {
+            publishDate = _publishDate;
+        }
+        //get set author
+        public String GetAuthor()
+        {
+            return author;
+        }
+        public void SetAuthor(String _author)
+        {
+            author = _author;
+        }
+
+        // get set content
+        public String GetContent()
+        {
+            return content;
+        }
+        public void SetContent(String _content)
+        {
+            content = _content;
+        }
+
+        //get averageRate
+        public float GetAverageRate()
+        {
+            return averageRate;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Title: " + GetTitle());
+            Console.WriteLine("Publish Date: " + GetPublishDate());
+            Console.WriteLine("Author: " + GetAuthor());
+            Console.WriteLine("Content: " + GetContent());
+            Console.WriteLine("Average Rate: " + GetAverageRate());
+        }
+        public void caculate()
+        {
+            float tong = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                tong += rateList[i];
+            }
+            averageRate = tong / 3;
+            Console.WriteLine("Rate: " + averageRate);
+        }
+
+        public void EnterRate()
+        {
+            Console.WriteLine("Enter rate: ");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Rate [" + i + "]= ");
+                rateList[i] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
